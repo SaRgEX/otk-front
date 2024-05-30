0de6a02593518886f798af5f2a7d9757ab3b7528
SaRgEX <maze_98@mail.ru>
2024-05-30 18:53:31 +0500

    fix price chage on cart

--- static/script/cart.js
+++ static/script/cart.js
@@ -7,11 +7,11 @@ let productPrice = [];
 let cart_id = 0;
 let total = 0;
 let svgPath = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>`
-fetch('http://localhost:8080/api/cart/', {
+fetch('http://' + location.host + '/api/my/cart/', {
     method: "GET",
     headers: {
         'Content-Type': 'application/json', // тип содержимого
-        'Origin': 'http://localhost:8080', // разрешенный источник
+        'Origin': 'http://localhost:3000', // разрешенный источник
         'Authorization': `Bearer ${localStorage.getItem('token')}`
     },
 })
@@ -40,7 +40,7 @@ fetch('http://localhost:8080/api/cart/', {
             const amount = document.createElement('input');
             amount.type = Number;
             amount.className = "amount";
-            amount.onchange = () => scaleAmount(item.article, amount.valueAsNumber);
+            amount.onchange = () => onSelectionChange(item.article, amount.valueAsNumber);
             const plus = document.createElement('button');
             plus.className = "incrementButton";
             const minus = document.createElement('button');
@@ -87,12 +87,11 @@ function deleteFromCart(article) {
         "product_article": Number(article),
         cart_id: cart_id
     }
-    console.log(JSON.stringify(data))
-    fetch('http://localhost:8080/api/cart/' + article + '/', {
+    fetch('http://' + location.host + '/api/my/cart/' + article + '/', {
         method: "DELETE",
         headers: {
             'Content-Type': 'application/json',
-            'Origin': 'http://localhost:8080',
+            'Origin': 'http://localhost:3000',
             'Authorization': `Bearer ${localStorage.getItem('token')}`
         },
         body: JSON.stringify(data)
@@ -111,7 +110,7 @@ function deleteFromCart(article) {
 }
 
 function deleteProduct(id) {
-    total -= productPrice[productArticles.indexOf(Number(id))] * amountInputs[productArticles.indexOf(Number(id))].value / 100;
+    total -= productPrice[productArticles.indexOf(Number(id))] * amountInputs[productArticles.indexOf(Number(id))] / 100;
     deleteFromCart(id)
     updatePrice()
 }
@@ -123,6 +122,7 @@ function onInitializate() {
     incrementButtons.forEach((button, index) => {
         button.addEventListener('click', () => {
             inputs[index].valueAsNumber++;
+            amountInputs[index]++;
             scaleAmount(productArticles[index], inputs[index].valueAsNumber);
             total += productPrice[index] / 100;
             updatePrice();
@@ -131,20 +131,31 @@ function onInitializate() {
 
     decrementButtons.forEach((button, index) => {
         button.addEventListener('click', () => {
-            if (amountInputs[index] > 1) {
-                inputs[index].valueAsNumber = amountInputs[index] -= 1;
+            if (inputs[index].valueAsNumber > 1) {
+                inputs[index].valueAsNumber--;
+                amountInputs[index]--;
                 scaleAmount(productArticles[index], inputs[index].valueAsNumber);
+                total -= productPrice[index] / 100;
+                updatePrice();
             }
             else {
-                deleteProduct(productArticles[index]);
+                total -= productPrice[index] / 100;
+                console.log(total)
+                deleteFromCart(productArticles[index]);
+                updatePrice()
             }
-            total -= productPrice[index] / 100;
-            updatePrice();
         });
     });
     amountInputs.forEach((index) => {
         refreshTotal();
     });
+}
+
+function onSelectionChange(article, amount) {
+    amountInputs[productArticles.indexOf(Number(article))] = amount;
+    total = amount * productPrice[productArticles.indexOf(Number(article))] / 100;
+    updatePrice();
+    scaleAmount(productArticles[productArticles.indexOf(Number(article))], amount);
 }
 
 function refreshTotal() {
@@ -162,24 +173,21 @@ function scaleAmount(article, amount) {
         cart_id: cart_id,
         amount: amount
     }
-    fetch('http://localhost:8080/api/cart/', {
+    fetch('http://' + location.host + '/api/my/cart/', {
         method: "PUT",
         headers: {
             'Content-Type': 'application/json',
-            'Origin': 'http://localhost:8080',
+            'Origin': 'http://localhost:3000',
             'Authorization': `Bearer ${localStorage.getItem('token')}`
         },
         body: JSON.stringify(data)
     })
         .then(response => {
-            if (response.status === 200) {
-                console.log("OK")
-            }
+            if (response.status === 200) {}
         })
 }
 
 function updatePrice() {
-    console.log(total)
     totalPrice.innerHTML = "Итого: " + total + " ₽"   
 }
 
@@ -202,7 +210,6 @@ function createOrder() {
     const address = document.getElementById('address-selector')
     console.log(address)
     const address_id = address.options[address.selectedIndex].id
-    console.log(address_id)
     purchase = [];
     for(let i = 0; i < amountInputs.length; i++) {
         purchase = purchase.concat({product_article: productArticles[i], amount: amountInputs[i].valueAsNumber})
@@ -211,11 +218,11 @@ function createOrder() {
         "purchase": purchase,
         "address": parseInt(address_id)
     }
-    fetch('http://localhost:8080/api/order/', {
+    fetch('http://' + location.host + '/api/my/order/', {
         method: "POST",
         headers: {
             'Content-Type': 'application/json',
-            'Origin': 'http://localhost:8080',
+            'Origin': 'http://localhost:3000',
             'Authorization': `Bearer ${localStorage.getItem('token')}`
         },
         body: JSON.stringify(data)
@@ -230,7 +237,7 @@ function createOrder() {
 }
 
 function getAddress() {
-    fetch('http://localhost:8080/api/address/', {
+    fetch('http://' + location.host + '/api/my/address/', {
         headers: {
             'Authorization': `Bearer ${localStorage.getItem('token')}`
         },
@@ -249,11 +256,11 @@ function getAddress() {
 }
 
 function removeAllFromCart() {
-    fetch('http://localhost:8080/api/cart/', {
+    fetch('http://' + location.host + '/api/my/cart/', {
         method: "DELETE",
         headers: {
             'Content-Type': 'application/json',
-            'Origin': 'http://localhost:8080',
+            'Origin': 'http://localhost:3000',
             'Authorization': `Bearer ${localStorage.getItem('token')}`
         },
     })
